0cdd439247afca42e5545b9dbdfea7f328a11f2e
Milos <milos.kovacevic.101@hotmail.com>
2020-02-18 22:07:25 +0100

    Added changes on landing page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Yo! Njau ! Skrr Shu
+          Edit <code>src/App.js</code> and save to reload. Yo! Njau ! Feature Branch
         </p>
         <a
           className="App-link"
